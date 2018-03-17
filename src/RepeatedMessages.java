61f4da84058b991523c269631fc336e8d8d84577
sniperunit234 <37313753+sniperunit234@users.noreply.github.com>
2018-03-17 11:28:30 -0400

    repeated messages
    This was cool and fun

---
+++ src/RepeatedMessages.java
@@ -0,0 +1,10 @@
+
+public class RepeatedMessages {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <=1000; i++) {
+			System.out.println("Milhouse did not test cootie positive");
+		}
+	}
+}	
+/* this was very cool to do */
